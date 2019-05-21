09060b8aef7aeaa986aa6e85d31980fb745e0e38
AT <cernunos_kav@msn.com>
2019-05-21 15:14:52 +0300

    init commit

---
+++ src/Client/ErrorBoundaries.fs
@@ -0,0 +1,40 @@
+
+// based on https://github.com/fable-compiler/fable-react/blob/master/docs/react-error-boundaries.md
+module ReactErrorBoundary 
+    open Fable.React
+
+    type [<AllowNullLiteral>] InfoComponentObject =
+        abstract componentStack: string with get
+
+    type ErrorBoundaryProps =
+        { Inner : ReactElement
+          ErrorComponent : ReactElement
+          OnError : exn * InfoComponentObject -> unit }
+
+    type ErrorBoundaryState =
+        { HasErrors : bool }
+
+    // See https://github.com/MangelMaxime/Fulma/blob/master/docs/src/Widgets/Showcase.fs
+    // See https://reactjs.org/docs/error-boundaries.html
+    type ErrorBoundary(props) =
+        inherit Component<ErrorBoundaryProps, ErrorBoundaryState>(props)
+        do base.setInitState({ HasErrors = false })
+
+        override x.componentDidCatch(error, info) =
+            let info = info :?> InfoComponentObject
+            x.props.OnError(error, info)
+            x.setState(fun state props -> {state with HasErrors = true })
+
+        override x.render() =
+            if (x.state.HasErrors) then
+                x.props.ErrorComponent
+            else
+                x.props.Inner
+
+    let renderCatchSimple errorElement element =
+        Browser.Dom.console.info("renderCatchSimple is called")
+        ofType<ErrorBoundary,_,_> { Inner = element; ErrorComponent = errorElement; OnError = ignore } [ ]
+
+    let renderCatchFn onError errorElement element =
+        Browser.Dom.console.info("renderCatchFn is called")
+        ofType<ErrorBoundary,_,_> { Inner = element; ErrorComponent = errorElement; OnError = onError } [ ]
